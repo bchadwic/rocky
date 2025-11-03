75d8e38fb2d782de147804a5d479172a24549e85
bchadwic <benchadwick87@gmail.com>
2025-11-02 21:22:00 -0800

    testing

--- main.c
+++ main.c
@@ -1,58 +1,72 @@
 #include <stdio.h>
-#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <unistd.h>
-#include "../include/stun.h"
-#include "../include/peer.h"
-#include "../include/holepunch.h"
+#include <pthread.h>
 
-int rocky(int fd)
+// GOAL: transmit a UDP packet reliably to a peer
+
+extern int odon_socket(struct sockaddr_in *local, socklen_t len);
+extern int odon_send(int fd, char *buf, size_t len);
+
+static void *await_ack(void *arg);
+
+int main(void)
 {
     struct sockaddr_in local = {0};
     local.sin_family = AF_INET;
     local.sin_addr.s_addr = INADDR_ANY;
-    local.sin_port = htons(ROCKY_PORT);
+    local.sin_port = htons(58888);
 
-    if (bind(fd, (struct sockaddr *)&local, sizeof(local)) == -1)
+    int fd = odon_socket(&local, sizeof(local));
+    if (fd < 0)
     {
-        return -1;
+        return 1;
     }
 
-    struct sockaddr_in public_addr = {0};
-    if (getpublicaddress(fd, &public_addr) == -1)
-    {
-        return -1;
-    }
-    printf("public: %s:%d\n", inet_ntoa(public_addr.sin_addr), public_addr.sin_port);
+    char *buf = "hello world\n";
+    size_t len = sizeof(buf);
 
-    struct sockaddr_in peer_addr = {0};
-    if (getpeeraddress(&peer_addr) == -1)
+    if (odon_send(fd, buf, len) < 0)
     {
-        return -1;
+        return 1;
     }
-
-    if (holepunch(fd, &peer_addr) == -1)
-    {
-        return -1;
-    }
-
+    close(fd);
     return 0;
 }
 
-int main(void)
+int odon_socket(struct sockaddr_in *local, socklen_t len)
 {
-    int fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (fd == -1)
+    int fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+    if (fd < 0)
     {
         return -1;
     }
 
-    int res = rocky(fd);
-    close(fd);
-
-    if (res == -1)
+    if (bind(fd, (struct sockaddr *)local, len) < 0)
     {
-        perror("failed");
-        return 1;
+        return -1;
     }
+
+    return fd;
+}
+
+int odon_send(int fd, char *buf, size_t len)
+{
+    pthread_t verify;
+    pthread_create(&verify, NULL, await_ack, fd);
+
+    // send UDP packet
+
+    pthread_join(verify, NULL);
     return 0;
+}
+
+static void *await_ack(void *arg)
+{
+    int fd = (int)arg;
+
+    // listen for ack
+
+    return NULL;
 }
