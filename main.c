a414ce4c9a785c782b5681b8ce6e61aed245efee
Ben Chadwick <benchadwick87@gmail.com>
2025-11-03 20:16:02 -0800

    testing

--- main.c
+++ main.c
@@ -22,22 +22,38 @@ extern int odon_init(struct odon_conn *conn,
                      struct sockaddr_in *src, socklen_t src_len,
                      struct sockaddr_in *dst, socklen_t dst_len);
 extern int odon_send(struct odon_conn *conn, char *buf, size_t len);
+extern int odon_recv(struct odon_conn *conn, char *buf, size_t len);
 // should be called after every odon_* function that fails
 extern void odon_free(struct odon_conn *conn);
 
 static void *await_ack(void *arg);
 
-int main(void)
+int main(int argc, char *argv[])
 {
+    if (argc < 4)
+    {
+        printf("need 3 arguments\n");
+        return 1;
+    }
+
+    char *arg = argv[1];
+    int src_port = atoi(argv[2]);
+    int dst_port = atoi(argv[3]);
+    if (src_port < 0 || dst_port < 0)
+    {
+        printf("arguments must be numeric\n");
+        return 1;
+    }
+
     struct sockaddr_in src = {0};
     src.sin_family = AF_INET;
     src.sin_addr.s_addr = htonl(INADDR_ANY);
-    src.sin_port = htons(58888);
+    src.sin_port = htons(src_port);
 
     struct sockaddr_in dst = {0};
     dst.sin_family = AF_INET;
     dst.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-    dst.sin_port = htons(58887);
+    dst.sin_port = htons(dst_port);
 
     struct odon_conn conn = {0};
     if (odon_init(&conn, &src, sizeof(src), &dst, sizeof(dst)) < 0)
@@ -47,14 +63,28 @@ int main(void)
         return 1;
     }
 
-    char *buf = "hello world\n";
-    size_t len = strlen(buf);
-
-    if (odon_send(&conn, buf, len) < 0)
+    if (strcmp(arg, "send") == 0)
     {
-        odon_free(&conn);
-        perror("odon_send");
-        return 1;
+        char *buf = "hello world\n";
+        size_t len = strlen(buf);
+
+        if (odon_send(&conn, buf, len) < 0)
+        {
+            odon_free(&conn);
+            perror("odon_send");
+            return 1;
+        }
+    }
+    else
+    {
+        char buf[512] = {0};
+        if (odon_recv(&conn, buf, 512) < 0)
+        {
+            odon_free(&conn);
+            perror("odon_recv");
+            return -1;
+        }
+        printf("recv: %s\n", buf);
     }
 
     odon_free(&conn);
@@ -129,6 +159,22 @@ extern int odon_send(struct odon_conn *conn, char *buf, size_t len)
     }
 
     pthread_join(tid, NULL);
+    return 0;
+}
+
+extern int odon_recv(struct odon_conn *conn, char *buf, size_t len)
+{
+    if (read(conn->socket, buf, len) < 0)
+    {
+        return -1;
+    }
+
+    uint8_t *ack[512];
+    if (write(conn->socket, ack, 512) < 0)
+    {
+        return -1;
+    }
+
     return 0;
 }
 
