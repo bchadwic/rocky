daae1e5f4ea07e4e3f32f9669bb05dac2a8a598f
Ben Chadwick <benchadwick87@gmail.com>
2025-11-05 20:21:24 -0800

    cleaning up for code review

--- main.c
+++ main.c
@@ -1,63 +1,16 @@
 #include "./include/odon.h"
 
-int run(int argc, char *argv[]);
-int send_cmd(struct sockaddr_in *src,
-             socklen_t src_len,
-             struct sockaddr_in *dst,
-             socklen_t dst_len);
-int recv_cmd(struct sockaddr_in *src,
-             socklen_t src_len,
-             struct sockaddr_in *dst,
-             socklen_t dst_len);
+static int run(int argc, char *argv[]);
+static int send_cmd(struct sockaddr_in *src, socklen_t src_len, struct sockaddr_in *dst, socklen_t dst_len);
+static int recv_cmd(struct sockaddr_in *src, socklen_t src_len, struct sockaddr_in *dst, socklen_t dst_len);
 
-int send_cmd(struct sockaddr_in *src,
-             socklen_t src_len,
-             struct sockaddr_in *dst,
-             socklen_t dst_len)
+int main(int argc, char *argv[])
 {
-
-    printf("sending...\n");
-    char *buf = "hello world";
-    size_t len = strlen(buf);
-
-    struct odon_conn conn = {0};
-    if (odon_init(&conn, src, src_len, dst, dst_len) < 0)
+    if (run(argc, argv) < 0)
     {
-        odon_free(&conn);
-        return -1;
+        perror("run");
+        return 1;
     }
-
-    if (odon_send(&conn, buf, len) < 0)
-    {
-        odon_free(&conn);
-        return -1;
-    }
-
-    odon_free(&conn);
-    return 0;
-}
-
-int recv_cmd(struct sockaddr_in *src,
-             socklen_t src_len,
-             struct sockaddr_in *dst,
-             socklen_t dst_len)
-{
-    printf("receiving...\n");
-    struct odon_conn conn = {0};
-    if (odon_init(&conn, src, src_len, dst, dst_len) < 0)
-    {
-        odon_free(&conn);
-        return -1;
-    }
-    char buf[512] = {0};
-    if (odon_recv(&conn, buf, 512) < 0)
-    {
-        odon_free(&conn);
-        return -1;
-    }
-
-    printf("recv: %s\n", buf);
-    odon_free(&conn);
     return 0;
 }
 
@@ -91,12 +44,47 @@ int run(int argc, char *argv[])
     }
 }
 
-int main(int argc, char *argv[])
+int send_cmd(struct sockaddr_in *src, socklen_t src_len, struct sockaddr_in *dst, socklen_t dst_len)
 {
-    if (run(argc, argv) < 0)
+
+    printf("sending...\n");
+    char *buf = "hello world";
+    size_t len = strlen(buf);
+
+    struct odon_conn conn = {0};
+    if (odon_init(&conn, src, src_len, dst, dst_len) < 0)
     {
-        perror("run");
-        return 1;
+        odon_free(&conn);
+        return -1;
     }
+
+    if (odon_send(&conn, buf, len) < 0)
+    {
+        odon_free(&conn);
+        return -1;
+    }
+
+    odon_free(&conn);
+    return 0;
+}
+
+int recv_cmd(struct sockaddr_in *src, socklen_t src_len, struct sockaddr_in *dst, socklen_t dst_len)
+{
+    printf("receiving...\n");
+    struct odon_conn conn = {0};
+    if (odon_init(&conn, src, src_len, dst, dst_len) < 0)
+    {
+        odon_free(&conn);
+        return -1;
+    }
+    char buf[512] = {0};
+    if (odon_recv(&conn, buf, 512) < 0)
+    {
+        odon_free(&conn);
+        return -1;
+    }
+
+    printf("recv: %s\n", buf);
+    odon_free(&conn);
     return 0;
 }
